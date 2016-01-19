8a705779d918778e092da17fb309a7a2bcf96c6c
Ryan Dobrinski <ryandobrinski@Macintosh-2.home>
2016-01-18 20:16:29 -0500

    added tests for dogs

--- spec/features/05users_add_dogs_spec.rb
+++ spec/features/05users_add_dogs_spec.rb
@@ -15,4 +15,18 @@ feature 'create dog objects through user page' do
     expect(page).to have_content("Bennett")
   end
 
+  scenario 'leaves breed field blank' do
+    user2 = FactoryGirl.create(:user)
+    visit root_path
+    click_link 'Sign In'
+    fill_in 'Email', with: user2.email
+    fill_in 'Password', with: user2.password
+    click_on 'Log in'
+    click_link 'Continue to User Profile'
+    click_link 'Add Dog'
+    fill_in 'Name', with: 'Bennett'
+    click_on 'Submit New Dog'
+    expect(page).to have_content("Breed can't be blank")
+  end
+
 end
