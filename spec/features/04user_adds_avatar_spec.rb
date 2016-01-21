6060f3f1e48cfa6bace3f850a59b4e1455e72ccf
Ryan Dobrinski <ryandobrinski@Macintosh-2.local>
2016-01-21 15:50:00 -0500

    implemented delete feature for memberships

--- spec/features/04user_adds_avatar_spec.rb
+++ spec/features/04user_adds_avatar_spec.rb
@@ -19,22 +19,4 @@ feature 'view index page see avatar' do
 
   end
 
-  scenario 'user creates account adds avatar' do
-    visit root_path
-    click_link 'Sign Up'
-    fill_in '* First name', with: "Ryan"
-    fill_in '* Last name', with: "Dobrinski"
-    fill_in '* Email', with: "ryandob@gmail.com"
-    fill_in 'Phone number', with: "5553210123"
-    fill_in '* Address', with: "22 This Street"
-    fill_in '* City', with: "Hopkinton"
-    fill_in '* State', with: "MA"
-    fill_in '* Zip', with: "01748"
-    fill_in '* Password', with: "password", match: :prefer_exact
-    fill_in '* Password confirmation', with: "password"
-    page.attach_file("Add Picture", "#{Rails.root}/spec/support/farley_default.png")
-    click_on 'Sign up'
-
-    expect(page).to have_content("Welcome! You have signed up successfully.")
-  end
 end
